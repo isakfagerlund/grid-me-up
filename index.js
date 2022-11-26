cc4866d92a4d8fa038ddcdccf060215e02aa2af7
Isak Fagerlund <fagerlund.isak@gmail.com>
2022-11-26 18:16:00 +0000

    Connect to a channel

--- index.js
+++ index.js
@@ -4,18 +4,6 @@ dotenv.config()
 import fetch from 'node-fetch';
 import { getGamesQuery } from "./queries.js";
 
-
-const client = new tmi.Client({
-  channels: ["runthefutmarket"],
-});
-
-client.connect();
-
-// Looking at all messages
-client.on("message", (_, tags, message) => {
-  console.log(`${tags["display-name"]}: ${message}`);
-});
-
 const getGamesFromYesterday = async (gamesQuery) => {
   const request = await fetch("https://api-op.grid.gg/central-data/graphql", {
     method: "POST",
@@ -31,11 +19,29 @@ const getGamesFromYesterday = async (gamesQuery) => {
   return await request.json();
 };
 
-const main = async () => {
-  const gamesQuery = getGamesQuery();
-  const gamesFromYesterday = await getGamesFromYesterday(gamesQuery);
-  const games = gamesFromYesterday.data.allSeries.edges
-  const gamesIds = games.map(game => game.node.id)
-  // All games Ids from yesterday
-  console.log(gamesIds)
-};
+const client = new tmi.Client({
+  channels: ["hoggiqq"],
+  identity: {
+    username: 'wiesaa',
+    password: '939oesn65i8l6pap4mwolquo6u9yro'
+  }
+});
+
+client.connect();
+
+// Looking at all messages
+client.on("message", async (channel, tags, message) => {
+  const username = tags["display-name"]
+  const isNotBot = username.toLowerCase() !== 'wiesaa';
+
+  if(isNotBot) {
+    if(message === '!gameId') {
+      const gamesQuery = getGamesQuery();
+      const gamesFromYesterday = await getGamesFromYesterday(gamesQuery);
+      const games = gamesFromYesterday.data.allSeries.edges
+      const gamesIds = games.map(game => game.node.id)
+
+      client.say(channel, `${gamesIds[0]}`);
+    }
+  }
+});
