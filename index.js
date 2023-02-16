c58726df62c10abd07b98cd60ab5cfca4a4afdf8
Isak Fagerlund <fagerlund.isak@gmail.com>
2023-02-16 10:30:32 +0100

    Add channel env

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import { getTopFragger } from './getTopFragger.js'
 import { getPreviousAndNextSeries } from './getNextAndPreviousSeries.js'
 
 const client = new tmi.Client({
-  channels: ["hoggiqq"],
+  channels: [process.env.TWITCH_CHANNEL],
   identity: {
     username: process.env.TWITCH_USERNAME,
     password: process.env.TWITCH_PASSWORD
